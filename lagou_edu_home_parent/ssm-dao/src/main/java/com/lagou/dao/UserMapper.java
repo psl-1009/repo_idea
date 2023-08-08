132438510bf64048c60a45963ae6a4e03c04766d
lagou_psl <lagou_psl@lagou.com>
2023-08-08 12:16:39 +0800

    update UserMapper

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -56,7 +56,13 @@ public interface UserMapper {
     
    public List<Resource> findResourceByRoleId2(ArrayList<Object> ids);
 
-
+   public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
+    public void test7();
 
 
 
